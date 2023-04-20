d352e9cd0df3bddb35886343060a4e06be02bbcf
Ashuthosh <63851543+Ashuthosh007@users.noreply.github.com>
2023-04-20 19:55:20 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -52,3 +52,19 @@ function check(){
     }
 
 }
+
+function validateForm() {
+    let x = document.forms["myForm"]["username"].value;
+    if (x == "") {
+        alert(" Username must be filled out");
+        return false;
+    }
+}
+
+function validateForm() {
+    let x = document.forms["myForm"]["password"].value;
+    if (x == "") {
+        alert("password must be filled out");
+        return false;
+    }
+}
